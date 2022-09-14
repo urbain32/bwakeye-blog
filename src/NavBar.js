d67af09de02e434bc29101ccc7754bc495bac94f
Mac <mac@Mac.local>
2022-09-14 14:35:40 +0200

    change json sve

--- src/NavBar.js
+++ src/NavBar.js
@@ -1,10 +1,15 @@
 import React from "react";
 import { Link } from "react-router-dom";
-
+import logo from './logo512.png'
 const NavBar = () => {
     return (
       <div className='navbar'>
-        <h1>Bwakeye</h1>
+        <div class='logo'>
+          <img src={logo} alt='' />
+          <h2>
+            BWA<span class='danger'>KEYE</span>
+          </h2>
+        </div>
         <div className='links'>
           <Link to='/'>Muhira</Link>
           <Link to='/create'>Shirako</Link>
